5e527b219b059d7237ebeb20fce53278a6a6d679
JammingJams <noahlogan20o00@gmail.com>
2026-05-08 13:41:44 -0400

    Dodged more black holes and added a more interface!

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class UserInterface {
     private Dealership dealership;
     public static Scanner sc = new Scanner(System.in);
+    public static UserInterface ui = new UserInterface();
 
     public UserInterface() {
         this.dealership = DealershipFileManger.getDealership();
@@ -12,44 +13,25 @@ public class UserInterface {
 
     public static void display() {
 
-        UserInterface ui = new UserInterface();
-
         boolean userInMainMenu = true;
 
         while(userInMainMenu) {
             System.out.print("Welcome to the ");
             System.out.println(ui.getDealership().getName());
-            System.out.print("Please select an option\n(1) -> Search vehicle Price\n" +
-                    "(2) -> Search vehicle Mileage\n" +
-                    "(3) -> Search vehicle Year\n" +
-                    "(4) -> Search vehicle Model\n" +
-                    "(5) -> Search vehicle Type\n" +
-                    "(6) -> Search vehicle color\n" +
-                    "(7) -> View all vehicles\n" +
-                    "(8) -> Add vehicle\n" +
-                    "(9) -> Remove vehicle\n" +
+            System.out.print("Please select an option\n" +
+                    "(1) -> View all vehicles\n" +
+                    "(2) -> Add vehicle\n" +
+                    "(3) -> Remove vehicle\n" +
                     "(X) -> Exits program\n");
 
             String userInput = sc.nextLine().toLowerCase().trim();
 
             switch (userInput) {
-                case ("1") -> ui.processGetByPriceRequest();
+                case ("1") -> ui.displayVehicles();
 
-                case ("2") -> ui.processGetByMileageRequest();
+                case ("2") -> ui.processGetAddVehiclesRequest();
 
-                case ("3") -> ui.processGetByYearRequest();
-
-                case ("4") -> ui.processGetByMakeModelRequest();
-
-                case ("5") -> ui.processGetByMakeColorRequest();
-
-                case ("6") -> ui.processGetByMakeVehicleTypeRequest();
-
-                case ("7") -> ui.processGetAllVehiclesRequest();
-
-                case ("8") -> ui.processGetAddVehiclesRequest();
-
-                case ("9") -> ui.processGetRemoveVehiclesRequest();
+                case ("3") -> ui.processGetRemoveVehiclesRequest();
 
                 case ("x") -> userInMainMenu = false;
 
@@ -77,7 +59,9 @@ public class UserInterface {
     }
 
     public void processGetAllVehiclesRequest() {
-
+        for (Vehicle v : dealership.getInventory()) {
+            System.out.printf("%d|%d|%s|%s|%s|%s|%d|%.2f\n",v.getVin(), v.getYear(), v.getMake(), v.getModel(), v.getVehicleType(), v.getColor(), v.getOdometer(), v.getPrice());
+        }
     }
 
     public void processGetAddVehiclesRequest() {
@@ -90,5 +74,42 @@ public class UserInterface {
 
     public Dealership getDealership() {
         return dealership;
+    }
+
+    public void displayVehicles() {
+        boolean userLoop = true;
+        while (userLoop) {
+            System.out.println("Please select an option\n" +
+                    "(1) -> Search vehicle Price\n" +
+                    "(2) -> Search vehicle Mileage\n" +
+                    "(3) -> Search vehicle Year\n" +
+                    "(4) -> Search vehicle Model\n" +
+                    "(5) -> Search vehicle Type\n" +
+                    "(6) -> Search vehicle color\n" +
+                    "(7) -> View all vehicles\n" +
+                    "(X) -> Exits to homepage");
+
+            String userInput = sc.nextLine().trim().toLowerCase();
+
+            switch (userInput) {
+                case ("1") -> ui.processGetByPriceRequest();
+
+                case ("2") -> ui.processGetByMileageRequest();
+
+                case ("3") -> ui.processGetByYearRequest();
+
+                case ("4") -> ui.processGetByMakeModelRequest();
+
+                case ("5") -> ui.processGetByMakeColorRequest();
+
+                case ("6") -> ui.processGetByMakeVehicleTypeRequest();
+
+                case ("7") -> ui.processGetAllVehiclesRequest();
+
+                case ("X") -> userLoop = false;
+
+                default -> System.out.println("Hey please don't input that...");
+            }
+        }
     }
 }
