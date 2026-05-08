96f8208df8ead5a36fb9a959d3eef933069965dc
JammingJams <noahlogan20o00@gmail.com>
2026-05-08 13:19:17 -0400

    Advoid creating infinite recurrsive loops and finally figured out how the static scanner works...

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,14 +1,61 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
     private Dealership dealership;
+    public static Scanner sc = new Scanner(System.in);
 
     public UserInterface() {
-
+        this.dealership = DealershipFileManger.getDealership();
     }
 
     public static void display() {
 
+        UserInterface ui = new UserInterface();
+
+        boolean userInMainMenu = true;
+
+        while(userInMainMenu) {
+            System.out.print("Welcome to the ");
+            System.out.println(ui.getDealership().getName());
+            System.out.print("Please select an option\n(1) -> Search vehicle Price\n" +
+                    "(2) -> Search vehicle Mileage\n" +
+                    "(3) -> Search vehicle Year\n" +
+                    "(4) -> Search vehicle Model\n" +
+                    "(5) -> Search vehicle Type\n" +
+                    "(6) -> Search vehicle color\n" +
+                    "(7) -> View all vehicles\n" +
+                    "(8) -> Add vehicle\n" +
+                    "(9) -> Remove vehicle\n" +
+                    "(X) -> Exits program\n");
+
+            String userInput = sc.nextLine().toLowerCase().trim();
+
+            switch (userInput) {
+                case ("1") -> ui.processGetByPriceRequest();
+
+                case ("2") -> ui.processGetByMileageRequest();
+
+                case ("3") -> ui.processGetByYearRequest();
+
+                case ("4") -> ui.processGetByMakeModelRequest();
+
+                case ("5") -> ui.processGetByMakeColorRequest();
+
+                case ("6") -> ui.processGetByMakeVehicleTypeRequest();
+
+                case ("7") -> ui.processGetAllVehiclesRequest();
+
+                case ("8") -> ui.processGetAddVehiclesRequest();
+
+                case ("9") -> ui.processGetRemoveVehiclesRequest();
+
+                case ("x") -> userInMainMenu = false;
+
+                default -> System.out.println("Hey type in one of the options");
+            }
+        }
     }
 
     public void processGetByPriceRequest() {
@@ -29,17 +76,19 @@ public class UserInterface {
     public void processGetByMakeVehicleTypeRequest() {
     }
 
-    public static void processGetAllVehiclesRequest() {
+    public void processGetAllVehiclesRequest() {
 
     }
 
-    public static void processGetAddVehiclesRequest() {
+    public void processGetAddVehiclesRequest() {
 
     }
 
-    public static void processGetRemoveVehiclesRequest() {
+    public void processGetRemoveVehiclesRequest() {
 
     }
 
-
+    public Dealership getDealership() {
+        return dealership;
+    }
 }
